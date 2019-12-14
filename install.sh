7fb94b5a543ba3e9d8e1a205555ebc5865572792
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-14 19:08:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,147 +32,13 @@ fi
 
 if [[ -d ~/camsnap ]]
 then
-sleep 0.5
-clear
-sleep 0.5
-printf "\e[1;92m   ____               \e[0m\e[1;77m ____                    \e[0m\n"
-printf "\e[1;92m  / ___|__ _ _ __ ___ \e[0m\e[1;77m/ ___| _ __   __ _ _ __  \e[0m\n"
-printf "\e[1;92m | |   / _\` | '_ \` _ \\\\\e[0m\e[1;77m\___ \| '_ \ / _\` | '_ \ \e[0m\n"
-printf "\e[1;92m | |__| (_| | | | | | |\e[0m\e[1;77m___) | | | | (_| | |_) |\e[0m\n"
-printf "\e[1;92m  \____\__,_|_| |_| |_|\e[0m\e[1;77m____/|_| |_|\__,_| .__/ \e[0m\n"
-printf "\e[1;92m                       \e[0m\e[1;77m                 |_|    \e[0m\n"
-echo
-
-if [[ -f /etc/camsnap.conf ]]
-then
-
-CONF="$( cat /etc/camsnap.conf )"
-sleep 1
-
-if [[ "$CONF" = "arm" ]]
-then
-if [[ -d /System/Library/CoreServices/SpringBoard.app ]]
-then
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-else 
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-pkg update
-pkg -y install curl
-pkg -y install php
-pkg -y install ssh
-pkg -y install unzip
-pkg -y install wget
-fi
-fi
-
-if [[ "$CONF" = "amd" ]]
-then
-if [[ -d /System/Library/CoreServices/Finder.app ]]
-then
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-else
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-apt-get update
-apt-get -y install curl
-apt-get -y install php
-apt-get -y install ssh
-apt-get -y install unzip
-apt-get -y install wget
-fi
-fi
-
-if [[ "$CONF" = "intel" ]]
-then
-if [[ -d /System/Library/CoreServices/Finder.app ]]
-then
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-else
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-apt-get update
-apt-get -y install curl
-apt-get -y install php
-apt-get -y install ssh
-apt-get -y install unzip
-apt-get -y install wget
-fi
-fi
-
-else
-read -e -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Select your architecture (amd/intel/arm): \e[0m' CONF
-if [[ "$CONF" = "" ]]
-then
-exit
-else
-if [[ "$CONF" = "arm" ]]
-then
-read -e -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Is this a single board computer (yes/no)? \e[0m' PI
-if [[ "$PI" = "yes" ]]
-then
-echo "amd" >> /etc/camsnap.conf
-CONF="amd"
-else
-echo "$CONF" >> /etc/camsnap.conf
-fi
-else
-echo "$CONF" >> /etc/camsnap.conf
-fi
-fi
-sleep 1
-
-if [[ "$CONF" = "arm" ]]
-then
-if [[ -d /System/Library/CoreServices/SpringBoard.app ]]
-then
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-else 
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-pkg update
-pkg -y install curl
-pkg -y install php
-pkg -y install ssh
-pkg -y install unzip
-pkg -y install wget
-fi
-fi
-
-if [[ "$CONF" = "amd" ]]
-then
-if [[ -d /System/Library/CoreServices/Finder.app ]]
-then
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-else
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-apt-get update
-apt-get -y install curl
-apt-get -y install php
-apt-get -y install ssh
-apt-get -y install unzip
-apt-get -y install wget
-fi
-fi
-
-if [[ "$CONF" = "intel" ]]
-then
-if [[ -d /System/Library/CoreServices/Finder.app ]]
-then
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-else
-echo -e "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Installing dependencies...\e[0m"
-apt-get update
-apt-get -y install curl
-apt-get -y install php
-apt-get -y install ssh
-apt-get -y install unzip
-apt-get -y install wget
-fi
-fi
-fi
-
+sleep 0
 else
 cd ~
 {
 git clone https://github.com/entynetproject/camsnap.git
 } &> /dev/null
+fi
 sleep 0.5
 clear
 sleep 0.5
@@ -239,7 +105,6 @@ fi
 fi
 
 else
-
 read -e -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Select your architecture (amd/intel/arm): \e[0m' CONF
 if [[ "$CONF" = "" ]]
 then
@@ -293,7 +158,7 @@ apt-get -y install wget
 fi
 fi
 
-if [[ "$CONF" = "amd" ]]
+if [[ "$CONF" = "intel" ]]
 then
 if [[ -d /System/Library/CoreServices/Finder.app ]]
 then
@@ -306,18 +171,16 @@ apt-get -y install php
 apt-get -y install ssh
 apt-get -y install unzip
 apt-get -y install wget
-fi
 fi
 fi
 fi
 
 {
 cd ~/camsnap/bin
-cp camsnap /bin
 cp camsnap /usr/local/bin
-chmod +x /bin/camsnap
 chmod +x /usr/local/bin/camsnap
+cp camsnap /bin
+chmod +x /bin/camsnap
 cp camsnap /data/data/com.termux/files/usr/bin
 chmod +x /data/data/com.termux/files/usr/bin/camsnap
-cd ~/camsnap
 } &> /dev/null
